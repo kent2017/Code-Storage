bf6001fa4d1469fee2dd60da18f806547a922426
kent2017 <kent2017kt@163.com>
2025-03-19 16:41:18 +0800

    Update Solution.cpp

--- QuickSort/Solution.cpp
+++ QuickSort/Solution.cpp
@@ -12,28 +12,28 @@ void split(int left, int right) {//Edit in a Java ide,but it can run in C++ and 
 		while ((a[i] <= C) && (i < j)) {
 			i++;
 		}
-			b = a[i];
-			a[i] = a[j];
-			a[j] = b;
-		}
-		b = a[d];
-		a[d] = a[j];
+		b = a[i];
+		a[i] = a[j];
 		a[j] = b;
-		if ((right > left) && (n > right)) {
-			int l1 = left;
-			int r1 = i - 1;
-			int l2 = i + 1;
-			int r2 = right;
-			if (r1 < left) {
-				r1 = left;
-			}
-			if (l2 > right) {
-				l2 = right;
-			}
-			split(l1, r1);
-			split(l2, r2);
-		}
 	}
+	b = a[d];
+	a[d] = a[j];
+	a[j] = b;
+	if ((right > left) && (n > right)) {
+		int l1 = left;
+		int r1 = i - 1;
+		int l2 = i + 1;
+		int r2 = right;
+		if (r1 < left) {
+			r1 = left;
+		}
+		if (l2 > right) {
+			l2 = right;
+		}
+		split(l1, r1);
+		split(l2, r2);
+	}
+}
 int main(){
 	cin>>n;
 	for (int k = 0; k < n; k++) {
