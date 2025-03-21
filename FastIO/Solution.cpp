a09d4ddf557e8dca587c387c188be0121bde729a
kent2017 <kent2017kt@163.com>
2025-03-21 16:45:29 +0800

    Add files via upload

---
+++ FastIO/Solution.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int read(){
+    char ch=getchar();
+    int f=1,val=0;
+    for(;ch!='-'&&(ch>'9'||ch<'0');ch=getchar());
+    if(ch=='-'){f=-1;ch=getchar();}
+    for(;ch<='9'&&ch>='0';ch=getchar()){
+        val*=10;val+=ch-'0';
+    }
+    return val*f;
+}
+void write(int x){
+	if(x<0){
+		x*=-1;putchar('-');
+	}
+	int cnts[50],cnt=0;
+	for(;x;x/=10,++cnt){
+		cnts[cnt]=x%10;
+	}
+	for(int i=cnt-1;i>=0;--i){
+		putchar('0'+cnts[i]);
+	}
+	return;
+} 
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int n=read(),k=read();
+    for(;--n;){
+        k+=read();
+    }
+    write(k);
+	return 0;
+}
+
