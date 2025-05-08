6b1b4dab2746a2968d75fa2d6b2cb513e3ca626b
kent2017 <kent2017kt@163.com>
2025-05-08 14:47:25 +0800

    Update Solution.cpp

--- FastIO/Solution.cpp
+++ FastIO/Solution.cpp
@@ -1,35 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-int read(){
-    char ch=getchar();
-    int f=1,val=0;
+int input(){
+    char ch=getchar();int f=1,val=0;
     for(;ch!='-'&&(ch>'9'||ch<'0');ch=getchar());
     if(ch=='-'){f=-1;ch=getchar();}
     for(;ch<='9'&&ch>='0';ch=getchar()){
-        val*=10;val+=ch-'0';
+		val*=10;val+=ch-'0';
     }
     return val*f;
 }
-void write(int x){
-	if(x<0){
-		x*=-1;putchar('-');
-	}
-	if(!x){
-		putchar('0');return; 
-	}
+void output(int x){
+	if(x<0){x*=-1;putchar('-');}
+	if(!x){putchar('0');return; }
 	int cnts[50],cnt=0;
-	for(;x;x/=10,++cnt){
-		cnts[cnt]=x%10;
-	}
-	for(int i=cnt-1;i>=0;--i){
-		putchar('0'+cnts[i]);
-	}
+	for(;x;x/=10,++cnt){cnts[cnt]=x%10;}
+	for(int i=cnt-1;i>=0;--i){putchar('0'+cnts[i]);}
 	return;
 } 
 int main(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);cout.tie(0);
 	
 	return 0;
 }
-
