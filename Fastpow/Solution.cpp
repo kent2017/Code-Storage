a173ffa1a845060f1e528238cfcd9b34d75ce35b
kent2017 <kent2017kt@163.com>
2025-07-12 10:15:56 +0800

    Update Solution.cpp

--- Fastpow/Solution.cpp
+++ Fastpow/Solution.cpp
@@ -1,36 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long mod;
-long long fastpow1(long long Base,long long index){
-	long long tans=1,fans=Base;
+long long mod,a,b;
+template<class T> T fastpow(T Base, long long index) {
+	T tans=1,fans=Base;
 	for(long long i=1;i<=index;i<<=1){
-//		cout<<fans<<" "<<tans<<" "<<i<<" "<<(i&index)<<endl;
 		if(i&index){
-			tans*=fans;
+			tans=(tans*fans)%mod;
 		}
-		tans%=mod;fans*=fans;fans%=mod;
+		fans=(fans*fans)%mod;
 	}
-	return tans%mod;
-} 
-long long fastpow2(long long Base,int index){
-	if(index==1){
-		return Base;
-	}
-	if(index==0){
-		return 1;
-	}
-	long long a=fastpow(Base,index/2)%p;
-	long long s=(a*a)%p;
-	if(index%2){
-		return s*Base%p;
-	}else{
-		return s;
-	}
-} 
+	return tans;
+}
 int main(){
-//	freopen("a.out","w",stdout);
-	long long a,b;
 	cin>>a>>b>>mod;
-	cout<<a<<"^"<<b<<" mod "<<mod<<"="<<fastpow1(a,b);
+	cout<<a<<"^"<<b<<" mod "<<mod<<"="<<fastpow(a,b)<<endl;
 	return 0;
 }
