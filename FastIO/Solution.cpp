f8558f0ba0370baa865a9456a0897bc00e5867f9
kent2017 <kent2017kt@163.com>
2025-07-12 08:56:23 +0800

    Update Solution.cpp

--- FastIO/Solution.cpp
+++ FastIO/Solution.cpp
@@ -1,22 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-// int input(){
-//     char ch=getchar();int f=1,val=0;
-//     for(;ch!='-'&&(ch>'9'||ch<'0');ch=getchar());
-//     if(ch=='-'){f=-1;ch=getchar();}
-//     for(;ch<='9'&&ch>='0';ch=getchar()){val*=10;val+=ch-'0';}
-//     return val*f;
-// }
-// void output(int x){
-// 	if(x<0){x*=-1;putchar('-');}
-// 	if(!x){putchar('0');return; }
-// 	int cnts[50],cnt=0;
-// 	for(;x;x/=10,++cnt){cnts[cnt]=x%10;}
-// 	for(int i=cnt-1;i>=0;--i){putchar('0'+cnts[i]);}
-// 	return;
-// } 
-#include<bits/stdc++.h>
-using namespace std;
 template<typename T> typename std::enable_if<std::is_integral<T>::value>::type input(T &x){
     x=0;bool f=0;char c=getchar();
     for(;c!='-'&&(c<'0'||c>'9');c=getchar());
@@ -59,7 +42,7 @@ template<> inline void read</*Your type*/>(/*Your type*/ &x) {
 }
 template<> inline void output</*Your type*/>(const /*Your type*/ &x) {
 	//Your code
-
+}
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);cout.tie(0);
